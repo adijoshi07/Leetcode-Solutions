6958bf2ec23b8e7d7d9a3b1d50cd0d5c0eceee1e
adijoshi07 <aditijoshi25510@gmail.com>
2025-10-27 14:12:01 +0530

    Add inorder traversal function for binary tree
    Implement inorder traversal for binary tree.

---
+++ inorder_traversal.cpp
@@ -0,0 +1,42 @@
+// 94. Binary Tree Inorder Traversal
+// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+// Example 1:
+
+// Input: root = [1,null,2,3]
+
+// Output: [1,3,2]
+
+// Explanation:
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> result;
+    inorderHelper(root, result);
+    return result;
+}
+
+void inorderHelper(TreeNode* node, vector<int>& result) {
+    if (!node) return;
+    inorderHelper(node->left, result);
+    result.push_back(node->val);
+    inorderHelper(node->right, result);
+}
+
+};
