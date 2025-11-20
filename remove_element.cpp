57583d76883dcced1ee64a8030453b3de9fbd431
adijoshi07 <aditijoshi25510@gmail.com>
2025-11-20 15:34:28 +0530

    Add comments to remove_element.cpp for clarity
    Added comments to explain the function and its approach.

--- remove_element.cpp
+++ remove_element.cpp
@@ -1,10 +1,34 @@
-Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
-The order of the elements may be changed.
-Then return the number of elements in nums which are not equal to val.
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+// The order of the elements may be changed.
+// Then return the number of elements in nums which are not equal to val.
 
-Example 1:
+// Example 1:
 
-Input: nums = [3,2,2,3], val = 3
-Output: 2, nums = [2,2,_,_]
-Explanation: Your function should return k = 2, with the first two elements of nums being 2.
-It does not matter what you leave beyond the returned k (hence they are underscores).
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+//Approach - used two pointers
+//time complexity - O(n)
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int left = 0, right = nums.size()-1;
+        while(left <= right){
+            if(nums[left] == val){
+                if(nums[right] != val){
+                    swap(nums[left],nums[right]);
+                    left ++;
+                }
+                right --;
+            }
+            else{
+                left ++;
+            }
+        }
+        return left;
+    }
+};
