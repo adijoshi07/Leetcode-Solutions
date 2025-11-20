0a8b2d099444be2ec485a392a16ccb1f8c8f121a
adijoshi07 <aditijoshi25510@gmail.com>
2025-11-20 15:43:04 +0530

    Add files via upload

---
+++ reverse_integer.cpp
@@ -0,0 +1,33 @@
+//Given a signed 32-bit integer x, return x with its digits reversed. 
+//If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+// Example 1:
+
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
+
+//time complexity - O(1)
+
+class Solution {
+public:
+int reverse(int x) {
+    int rev = 0;
+    while (x != 0) {
+        int digit = x % 10;
+        if(( rev  > INT_MAX/10) || ( rev < INT_MIN/10)){
+            return 0;
+        }
+        rev = rev * 10 + digit;
+        x /= 10;
+    }
+    return rev;
+}
+};
