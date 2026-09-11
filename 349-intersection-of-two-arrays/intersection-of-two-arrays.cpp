d49d21a185d7a87fe916107c0320e92faa583ae4
adijoshi07 <aditijoshi25510@gmail.com>
2026-09-11 17:16:43 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (24.54%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,31 +1,13 @@
 class Solution {
 public:
-    bool binarysearch(vector<int>& nums, int target){
-        int l = 0, r = nums.size()-1;
-        while(l <= r){
-            int mid = l + (r-l)/2;
-            if(nums[mid] == target){
-                return true;
-            }
-            else if(nums[mid] < target){
-                l = mid + 1;
-            }
-            else{
-                r = mid-1;
-            }
-        }
-        return false;
-
-    }
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end());
-        unordered_set <int> s;
+        unordered_set<int>s1(nums1.begin(), nums1.end());
+        unordered_set<int>res;
         for(int num : nums2){
-            if(binarysearch(nums1, num)){
-                s.insert(num);
+            if(s1.count(num)){
+                res.insert(num);
             }
         }
-        vector<int> res(s.begin(), s.end());
-        return res;
+        return vector<int>(res.begin(), res.end());
     }
 };
